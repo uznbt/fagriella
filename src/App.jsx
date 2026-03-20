626377556f4bac3ce35426c554083f2cd5087a03
arsipkuliah <kelasfagribisnis@gmail.com>
2026-03-20 10:05:27 +0700

    update notification v2

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import PanduanUpload from './pages/PanduanUpload';
 import Pengaturan from './pages/Pengaturan';
 import Dokumentasi from './pages/Dokumentasi';
 import ResultView from './pages/ResultView';
+import Admin from './pages/Admin';
 
 // ... other pages
 
@@ -36,6 +37,7 @@ function App() {
                     <Route path="/tentang" element={<Tentang />} />
                     <Route path="/panduan-upload" element={<PanduanUpload />} />
                     <Route path="/pengaturan" element={<Pengaturan />} />
+                    <Route path="/admin" element={<Admin />} />
 
                     {/* Home routes with dynamic params */}
                     <Route path="/:smtParam" element={<Home />} />
